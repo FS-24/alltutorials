0c7607a06b9390fb1ee3d792cd674ecb68307542
Hippolyte EZIAN <polarusdev@gmail.com>
2022-04-07 14:02:03 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const element = <h1>Hello Ablam</h1>;
-ReactDOM.createRoot(document.querySelector("#container")).render(element);
+
+// https/jckinter.com/wp-content/uploads/2020/12/bistella-300f.jpg
+
+let produit = {
+  nom: "bistella",
+  image: "https://jckinter.com/wp-content/uploads/2020/12/bistella-300f.jpg",
+  qte: 200,
+  categorie: "biscuit",
+  promotion: false,
+};
+
+function Expo(props) {
+  return (
+    <div>
+      <img src={props.produit.image} width="200px" alt="biscuit" />
+      <h1>Nom : {props.produit.nom}</h1>
+      <h2>Qte : {props.produit.qte}</h2>
+      <h2>Categorie : {props.produit.categorie}</h2>
+      <h2>En promotion {props.produit.promotion ? "oui" : "non"}</h2>
+    </div>
+  );
+}
+
+function App() {
+  return <div></div>;
+}
+
+ReactDOM.createRoot(document.querySelector("#container")).render(
+  <Expo produit={produit} />
+);
