fa09a2e0f71be89496ed99dcc40485f53c13a42c
Hippolyte EZIAN <polarusdev@gmail.com>
2022-04-08 10:18:50 +0100

    fisrt commit on hooks

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const element = <h1>Hello Ablam</h1>;
+const element = <h1>Hello Hooks</h1>;
 ReactDOM.createRoot(document.querySelector("#container")).render(element);
