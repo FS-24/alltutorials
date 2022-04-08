5f19afb6f63834b8127d46a5c7153067edbde5cd
Hippolyte EZIAN <polarusdev@gmail.com>
2022-04-08 14:57:46 +0100

    Todos with useEffect

--- src/index.js
+++ src/index.js
@@ -1,43 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 
-function Addition() {
-  const [values, setValues] = useState([0, 0]);
+function Todo() {
+  const [todos, setTodos] = useState([]);
 
-  const [somme, setsomme] = useState(0);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((respone) => respone.json())
+      .then((data) => {
+        setTodos(data);
+      });
+  }, []);
 
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        setsomme(values[0] + values[1]);
-      }}
-    >
-      <input
-        type="number"
-        name="value1"
-        value={values[0]}
-        placeholder="Enter first value"
-        onChange={(e) => {
-          setValues([Number(e.target.value), values[1]]);
-        }}
-      />
-      +
-      <input
-        type="number"
-        name="value2"
-        value={values[1]}
-        placeholder="Enter Second value"
-        onChange={(e) => {
-          setValues([values[0], Number(e.target.value)]);
-        }}
-      />
-      <span>La somme est : {somme} </span>
-      <div>
-        <input type="submit" value="Calculer" />
-      </div>
-    </form>
+    <ul>
+      {todos.map((todo) => {
+        return <li key={todo.id}>{todo.title}</li>;
+      })}
+    </ul>
   );
 }
 
-ReactDOM.createRoot(document.querySelector("#container")).render(<Addition />);
+ReactDOM.createRoot(document.querySelector("#container")).render(<Todo />);
