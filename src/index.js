1ec4f1f02128a02e9e1803920a58f6737d44655f
Hippolyte EZIAN <polarusdev@gmail.com>
2022-04-09 11:04:36 +0100

    test

--- src/index.js
+++ src/index.js
@@ -1,4 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const element = <h1>Hello Ablam</h1>;
-ReactDOM.createRoot(document.querySelector("#container")).render(element);
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Acceuil</Link>
+          </li>
+          <li>
+            <Link to="/users">Utilisateurs</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+}
+
+function Home() {
+  return <div>Home</div>;
+}
+
+function Users() {
+  return <div>Users</div>;
+}
+
+function About() {
+  return <div>About</div>;
+}
+
+ReactDOM.createRoot(document.querySelector("#container")).render(<App />);
