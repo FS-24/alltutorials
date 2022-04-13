505c63294ec7e5adf681626b6569c8a72be0fc02
Hippolyte EZIAN <polarusdev@gmail.com>
2022-04-13 13:09:24 +0100

    add, list, delete users from firebase

--- src/index.js
+++ src/index.js
@@ -1,126 +1,165 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import {
-  addDoc,
-  collection,
-  deleteDoc,
-  deleteField,
-  doc,
-  getDocs,
   getFirestore,
+  collection,
+  getDocs,
+  addDoc,
   query,
-  serverTimestamp,
-  setDoc,
-  Timestamp,
-  updateDoc,
+  orderBy,
   where,
-} from "firebase/firestore";
+  deleteDoc,
+  doc,
+  Timestamp,
+} from "firebase/firestore/lite";
+import "bootstrap/dist/css/bootstrap.min.css";
 
+// Your web app's Firebase configuration
+// htmlFor Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDJuwOy8WH6OsnLCoUiuQnefsBqnP9I_CM",
-  authDomain: "fir-reactapp-17de5.firebaseapp.com",
-  projectId: "fir-reactapp-17de5",
-  storageBucket: "fir-reactapp-17de5.appspot.com",
-  messagingSenderId: "552383942457",
-  appId: "1:552383942457:web:15b780cbf38258e71122e8",
-  measurementId: "G-E3FMEQQPQ6",
+  apiKey: "AIzaSyB_Po5pufX5t3lREZ52oPuC0Tle2Acxjww",
+  authDomain: "usersmangementreact.firebaseapp.com",
+  projectId: "usersmangementreact",
+  storageBucket: "usersmangementreact.appspot.com",
+  messagingSenderId: "948310553292",
+  appId: "1:948310553292:web:1c129524adf2ea8e5b4869",
+  measurementId: "G-0H45XEL6RL",
 };
 
 // Initialize Firebase
-const initApp = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-// Initialize Cloud Firestore and get a reference to the service
-const db = getFirestore(initApp);
-
-// Add a new document in collection "cities"
-async function setData() {
-  // const cityRef = doc(db, "cities", "BJ");
-  // await setDoc(cityRef, { capital: true }, { merge: true });
-
-  const docData = {
-    stringExample: "Hello world!",
-    booleanExample: true,
-    numberExample: 3.14159265,
-    dateExample: Timestamp.fromDate(new Date("December 10, 1815")),
-    arrayExample: [5, true, "hello"],
-    nullExample: null,
-    objectExample: {
-      a: 5,
-      b: {
-        nested: "foo",
-      },
-    },
-  };
-
-  let ref = doc(collection(db, "data"));
-  await setDoc(ref, docData);
-  console.log(ref);
+async function addUser(user) {
+  // Add a new document with a generated id.
+  const docRef = await addDoc(collection(db, "users"), user);
+  console.log("Document written with ID: ", docRef.id);
 }
 
-async function addData() {
-  try {
-    const docRef = await addDoc(collection(db, "users"), {
-      first: "Ada",
-      last: "Lovelace",
-      born: 1815,
-    });
-    console.log("Document written with ID: ", docRef.id);
-  } catch (e) {
-    console.error("Error adding document: ", e);
+function App() {
+  const [users, setUsers] = useState([]);
+  const [update, setUpdate] = useState(false);
+  const [user, setUser] = useState({ name: "", email: "" });
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    let id = new Date().toUTCString().concat(Math.random() * 1000);
+    if (user.name.trim().length > 0 && user.email.trim().length > 0) {
+      // setUsers([...users, user]);
+      let newuser = {
+        id: id,
+        name: user.name,
+        email: user.email,
+        update: Timestamp.now(),
+      };
+      addUser(newuser);
+      setUser({ name: "", email: "" });
+    }
+    setUpdate(true);
+    // getUsers();
   }
+
+  async function getUsers() {
+    const q = query(collection(db, "users"), orderBy("update", "desc"));
+    const querySnapshot = await getDocs(q);
+    let userList = [];
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      // console.log(doc.id, " => ", doc.data());
+      userList.push(doc.data());
+    });
+    setUsers(userList);
+    setUpdate(false);
+  }
+
+  async function handleDelete(id) {
+    if (window.confirm("Voulez vous vraiment suppimer???")) {
+      const q = query(collection(db, "users"), where("id", "==", id));
+      const querySnapshot = await getDocs(q);
+      let docId;
+      try {
+        querySnapshot.forEach((doc) => {
+          // doc.data() is never undefined for query doc snapshots
+          docId = doc.id;
+        });
+
+        deleteDoc(doc(db, "users", docId)).then(() => {
+          setUpdate(true);
+          console.log("deleted");
+        });
+      } catch (error) {}
+    } else {
+      window.alert("suppression annuler");
+    }
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, [update]);
+
+  return (
+    <div className="container m-auto border shadow rounded mt-5 p-3">
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            value={user.name}
+            className="form-control"
+            onChange={(e) => {
+              setUser({ name: e.target.value, email: user.email });
+            }}
+            id="name"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            name="email"
+            value={user.email}
+            onChange={(e) => {
+              setUser({ name: user.name, email: e.target.value });
+            }}
+            aria-describedby="emailHelp"
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+
+      <div className="mt-3">
+        <ul className="list-group list-group">
+          {users.map((u) => {
+            return (
+              <li key={u.id} className="list-group-item">
+                <span className="d-flex justify-between">
+                  name: {u.name} email: {u.email}
+                  <button
+                    onClick={() => {
+                      handleDelete(u.id);
+                    }}
+                    className="ms-auto btn btn-sm btn-danger"
+                  >
+                    X
+                  </button>
+                </span>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 }
-
-async function updateData() {
-  const washingtonRef = doc(db, "cities", "LA");
-
-  // Set the "capital" field of the city 'DC'
-  await updateDoc(washingtonRef, {
-    name: "Los Angeless",
-    timestamp: serverTimestamp(),
-  });
-}
-
-async function deleteData() {
-  await deleteDoc(doc(db, "cities", "BJ"));
-}
-
-async function deleteFieldData() {
-  const cityRef = doc(db, "cities", "LA");
-
-  // Remove the 'capital' field from the document
-  await updateDoc(cityRef, {
-    capital: deleteField(),
-  });
-}
-
-async function deleteDocument() {
-  await deleteDoc(doc(db, "cities", "LA"));
-}
-
-async function getData() {
-  // const querySnapshot = await getDocs(collection(db, "users"));
-  const q = query(collection(db, "users"), where("born", "<", 1818));
-  const querySnapshot = await getDocs(q);
-  querySnapshot.forEach((doc) => {
-    // doc.data() is never undefined for query doc snapshots
-
-    let user = {
-      first: doc.data().first,
-      last: doc.data().last,
-    };
-
-    // console.log("Document ", doc.id, " => ", doc.data());
-    console.log("user", user);
-  });
-}
-
-// setData();
-// updateData();
-// deleteFieldData();
-// deleteDocument();
-getData();
-const element = <h1>Hello Ablam</h1>;
-ReactDOM.createRoot(document.querySelector("#container")).render(element);
+ReactDOM.createRoot(document.querySelector("#container")).render(<App />);
